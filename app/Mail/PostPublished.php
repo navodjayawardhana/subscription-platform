c274bcd01695bc2976e1aa1d77f6963e59a0f5ac
navod <navodthishan@gmail.com>
2025-09-22 14:44:57 +0530

    add retry support for post published mail

--- app/Mail/PostPublished.php
+++ app/Mail/PostPublished.php
@@ -4,14 +4,18 @@ namespace App\Mail;
 
 use App\Models\Post;
 use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
-class PostPublished extends Mailable
+class PostPublished extends Mailable implements ShouldQueue
 {
     use Queueable, SerializesModels;
 
     public Post $post;
+
+    public int $tries = 5;
+    public int $backoff = 60;
 
     public function __construct(Post $post)
     {
