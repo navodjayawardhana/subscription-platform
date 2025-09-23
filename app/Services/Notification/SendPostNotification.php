d658f40267c8800983c9cb8d5830be2b2aeaee40
navod <navodthishan@gmail.com>
2025-09-23 19:54:52 +0530

    Post notification Imediately send check test

--- app/Services/Notification/SendPostNotification.php
+++ app/Services/Notification/SendPostNotification.php
@@ -4,7 +4,10 @@ namespace App\Services\Notification;
 
 use App\Mail\PostPublished;
 use App\Models\Post;
+use App\Models\PostNotificationOutbox;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class SendPostNotification
 {
@@ -13,7 +16,29 @@ class SendPostNotification
         $subscribers = $post->website->subscribers;
 
         foreach ($subscribers as $subscriber) {
-            Mail::to($subscriber->email)->queue(new PostPublished($post));
+            $validator = Validator::make(['email' => $subscriber->email], ['email' => 'required|email']);
+            if ($validator->fails()) {
+                continue;
+            }
+            DB::beginTransaction();
+            try {
+                Mail::to($subscriber->email)->send(new PostPublished($post));
+                DB::commit();
+            } catch (\Throwable $e) {
+                DB::rollBack();
+                PostNotificationOutbox::updateOrCreate(
+                    [
+                        'post_id'       => $post->id,
+                        'subscriber_id' => $subscriber->id,
+                    ],
+                    [
+                        'email'          => $subscriber->email,
+                        'attempts'       => 0,
+                        'next_attempt_at'=> now()->addMinutes(5),
+                        'last_error'     => $e->getMessage(),
+                    ]
+                );
+            }
         }
     }
 }
